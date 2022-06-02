a54fee5150a9d7d170de196f2a2e42a89cb70262
kishanfowler <kv18226@ijburgcollege.nl>
2022-06-02 15:10:23 +0200

    audio toegevoegd aan de melee

--- Bo-Marine-Unity/Assets/kishanAssets/kishanScripts/swing.cs
+++ Bo-Marine-Unity/Assets/kishanAssets/kishanScripts/swing.cs
@@ -7,39 +7,38 @@ public class swing : MonoBehaviour
     Collision collision;
     [SerializeField] ParticleSystem particlesystem;
     internal bool hit;
-    [SerializeField] Animator animator;
-    private float timeStop = 1;
+    //[SerializeField] Animator animator;
     private bool ableToHit;
     internal float damage = 10f;
+    [SerializeField] AudioSource audioSource;
 
     void Start()
     {
         ableToHit = true;
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
         hit = false;
         particlesystem.Stop();
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            animator.SetBool("hit", true);
+            //animator.SetBool("hit", true);
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        audioSource.Play(0);
         target target = other.transform.GetComponent<target>();
         if (other.gameObject.tag == "enemy")
         {
             hit = true;
             particlesystem.Play();
-            Debug.Log(ableToHit);
             if (ableToHit == true)
             {
-                Debug.Log(ableToHit);
                 ableToHit = false;
             }
             target.takeDamage(damage);
